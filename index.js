ba7f1c361798058ffc9b30e69ff16467e416676d
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-10-02 19:00:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var new_array = [];
+  var i = 0;
+  for (var x in musicians){
+    new_array.push(musicians[x] + " plays " + instruments[i])
+    i = i + 1;
+  }
+  return new_array
+}
+
+function johnLennonFacts (array){
+  var x = 0 ;
+  while (x < array.length){
+    array[x]= (array[x] + "!!!");
+    x = x + 1
+  }
+  
+    
+  }
+  return array
+}
